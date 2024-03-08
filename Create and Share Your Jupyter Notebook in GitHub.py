4b4303de99447db5a95999209646e0174aa440eb
joancofan <162738309+joancofan@users.noreply.github.com>
2024-03-08 23:06:30 +0100

    Add files via upload

---
+++ Create and Share Your Jupyter Notebook in GitHub.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #!/usr/bin/env python
+# # coding: utf-8
+# 
+# # Data Science Tools and Ecosystem
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# - Python
+#  - Julia
+#  - R
+#  - Java
+#  - Swift
+#  - C/C++
+#  - SQL
+# 
+#  Some of the commonly used libraries used by Data Scientists include:
+#  - TensorFlow.
+#  - NumPy.
+#  - SciPy.
+#  - Pandas.
+#  - Matplotlib.
+#  - Keras.
+#  - SciKit-Learn.
+#  - PyTorch
+
+# | Data Science Tools |
+# | ------ |
+# | R Studio |
+# |Jupyter Notebook|
+# |Visual Studio Code|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[7]:
+
+
+result1 = 7-5
+print (result1)
+
+result2 = 7+5
+print (result2)
+
+result3 = 7*5
+print (result3)
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+
+# In[9]:
+
+
+result = (3*4)+5
+print (result)
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[16]:
+
+
+minutes = 200
+hours=minutes/60
+print(hours)
+
+
+# **Objectives:**
+# * List popular languages for Data Science
+# * Cloud based tools for data science
+# * Packages, API's, Datasets and Models
+# * Jupyter Notebooks and JupyterLabs
+# * R Studio and GitHub
+
+# ## Author
+# Joan Cofan
+
+# In[ ]:
+
+
+
+
